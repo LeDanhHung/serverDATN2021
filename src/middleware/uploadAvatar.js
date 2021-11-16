52563d2a870759299031f3076930810acdb4cfb9
LeDanhHung1 <hungmetxi@gmail.com>
2021-11-16 22:04:38 +0700

    hungfixx

--- src/middleware/uploadAvatar.js
+++ src/middleware/uploadAvatar.js
@@ -1,18 +1,18 @@
 import multer from 'multer';
-import path  from 'path';
+import path from 'path';
 import fs from 'fs';
 const __dirname = path.resolve();
-const uploadFile = path.resolve(path.join(__dirname, '../auth/public/uploads'));
+//const uploadFile = path.resolve(path.join(__dirname, '../auth/public/uploads'));
 
-if (!fs.existsSync(uploadFile)) {
-    fs.mkdirSync(uploadFile, { recursive: true });
-  }
-  const storage = multer.diskStorage({ 
-    destination: function(req,file,cb){
-      cb(null,path.join(__dirname,'../auth/public/uploads'));
+//if (!fs.existsSync(uploadFile)) {
+//   fs.mkdirSync(uploadFile, { recursive: true });
+//}
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        //    cb(null,path.join(__dirname,'../auth/public/uploads'));
     },
-    filename: function(req,file,cb){
-      cb(null,new Date().toISOString().replace(/:/g,'-')+file.originalname);
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
     }
-  })
-  export {storage}
+})
+export { storage }
